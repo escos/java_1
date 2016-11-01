3af7f3ff45d00c9fb051efd8d95684cb4e4adf29
escos <rv_kireev@bk.ru>
2016-11-01 13:02:50 +0300

    Final.ver2

--- MyTasks.java
+++ MyTasks.java
@@ -1,46 +1,45 @@
 import java.util.ArrayList;
 public class MyTasks {
     public static ArrayList createMyList() {
-        ArrayList<String> animals = new ArrayList();
-        animals.add("Волк");
-        animals.add("Заяц");
-        animals.add("Медведь");
-        animals.add("Лиса");
-        animals.add("Собака");
-        animals.add("Кот");
-        animals.add("Мышь");
-        System.out.println(animals);
-        System.out.println("Добавляем первый элемент в список: Лось ");
-        animals.add(0, "Лось");
-        System.out.println(animals);
-        return animals;
+        ArrayList<String> tasks = new ArrayList();
+        tasks.add("Задача №1");
+        tasks.add("Задача №2");
+        tasks.add("Задача №3");
+        tasks.add("Задача №4");
+        tasks.add("Задача №5");
+        tasks.add("Задача №6");
+        tasks.add("Задача №7");
+        System.out.println(tasks);
+        System.out.println("Добавляем первый элемент в список: дополнительная задача ");
+        tasks.add(0, "Дополнительная задача");
+        System.out.println(tasks);
+        return tasks;
     }
 
-    // Размер списка
-    public static void sizeMyList(ArrayList animals) {
-        animals.size();
-        System.out.println("Количество животных в списке " + animals.size());
+    // Размер списка задач
+    public static void sizeMyList(ArrayList tasks) {
+        tasks.size();
+        System.out.println("Количество задач в списке " + tasks.size());
     }
 
-    // Нахождение элемента по индексу
-    public static void getAnimalUseIndex(ArrayList animals) {
-        System.out.println("Второе с конца животное " + animals.get(animals.size() - 3));
-        int j = animals.indexOf("Лиса");
-        System.out.println("Индекс животного Лиса = " + j);
+    // Нахождение задачи по индексу
+    public static void getAnimalUseIndex(ArrayList tasks) {
+        System.out.println("Вторая задача с конца " + tasks.get(tasks.size()-3));
+        System.out.println("Индекс задачи = " + (tasks.size()-3));
     }
 
-    // Удаление элемента из списка
-    public static void deleteAnimal(ArrayList animals) {
-        System.out.println("Удаляем последний элемент ");
-        animals.remove(animals.size() - 1);
-        System.out.println(animals);
+    // Удаление задачи из списка
+    public static void deleteAnimal(ArrayList tasks) {
+        System.out.println("Удаляем последний элемент списка");
+        tasks.remove(tasks.size() - 1);
+        System.out.println(tasks);
     }
 
-    // Очистка списка
-    public static boolean listClear(ArrayList animals) {
-        animals.clear();
+    // Очистка списка задач
+    public static boolean listClear(ArrayList tasks) {
+        tasks.clear();
         boolean mes;
-        if (animals.size() == 0)
+        if (tasks.size() == 0)
             mes = true;
         else mes = false;
         System.out.println(mes);
