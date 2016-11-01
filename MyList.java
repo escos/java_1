026871eeeff73747e3bb370e169f005c3b2323c2
escos <rv_kireev@bk.ru>
2016-11-01 12:47:06 +0300

    Final.ver2

--- MyList.java
+++ MyList.java
@@ -1,54 +1,71 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MyList {
+
+    private String[] data = new String[16];
+    int size = 0;
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        ArrayList<String> a = createMyList();
-        sizeMyList(a);
-        getAnimalUseIndex(a);
-        deleteAnimal(a);
-        listClear(a);
+        MyList list = new MyList();
+        list.add("Запись №1");
+        System.out.println("Первая строка списка list.data[0] = " + list.data[0]);
+        list.add("Запись №2");
+        System.out.println("Вторая строка списка list.data[1] = " + list.data[1]);
+        System.out.println("Текущий размер списка = " + list.size());
+        list.add("Запись №3");
+        list.add("Запись №4");
+        list.add("Запись №5");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + "-й элемент списка = " + list.get(i));
+        }
+        System.out.println("Текущий размер списка = " + list.size());
+        System.out.println("Выберите номер строки списка для удаления(целое число от 1 до " + list.size + " )");
+        int j = sc.nextInt();
+        if ((j < list.size + 1) && (j > 0)) {
+            list.delete(j);
+            System.out.println("Получившийся список после удаления " + j + "-го элемента:");
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println((i + 1) + "-й элемент списка = " + list.get(i));
+            }
+        } else {
+            System.out.println("Не правильно введен номер элемента списка");
+        }
+        System.out.println("Очищаем список");
+        list.clear();
+        list.add("Новая запись №1!");
+        list.get(0);
+        System.out.println("Первый элемент списка = " + list.get(0));
     }
 
-    public static ArrayList createMyList() {
-        ArrayList<String> animals = new ArrayList();
-        animals.add("Волк");
-        animals.add("Заяц");
-        animals.add("Медведь");
-        animals.add("Лиса");
-        animals.add("Собака");
-        animals.add("Кот");
-        animals.add("Мышь");
-        System.out.println(animals);
-        System.out.println("Добавляем первый элемент в список: Лось ");
-        animals.add(0, "Лось");
-        System.out.println(animals);
-        return animals;
+    private int size() {
+        return size;
     }
-    // Размер списка
-    public static void sizeMyList(ArrayList animals) {
-        animals.size();
-        System.out.println("Количество животных в списке " + animals.size());
+
+    private String get(int i) {
+        return data[i];
     }
-    // Нахождение элемента по индексу
-    public static void getAnimalUseIndex(ArrayList animals) {
-        System.out.println("Второе с конца животное " + animals.get(animals.size()-3));
-        int j = animals.indexOf("Лиса");
-        System.out.println("Индекс животного Лиса = " + j);
+
+    private void add(String s) {
+        size++;
+        if (data.length < size) {
+            String[] newdata = new String[(int) (data.length * 1.5)];
+            for (int i = 0; i < newdata.length; i++) {
+                newdata[i] = data[i];
+            }
+            data = newdata;
+        }
+        data[size - 1] = s;
     }
-    // Удаление элемента из списка
-    public static void deleteAnimal(ArrayList animals) {
-        System.out.println("Удаляем последний элемент ");
-        animals.remove(animals.size()-1);
-        System.out.println(animals);
+
+    private void delete(int j) {
+        for (int i = j - 1; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        size--;
     }
-    // Очистка списка
-    public static boolean listClear(ArrayList animals) {
-        animals.clear();
-        boolean mes;
-        if (animals.size() == 0)
-            mes = true;
-        else mes = false;
-        System.out.println(mes);
-        return mes;
+
+    private void clear() {
+        size = 0;
     }
 }
